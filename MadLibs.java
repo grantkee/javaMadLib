103d7a767b2f635b0d262640f70edbb89a33058f
grantkee <grant.d.kee@gmail.com>
2020-06-21 13:30:50 -0500

    added another story and more variables. fun to play with friends and family.

--- MadLibs.java
+++ MadLibs.java
@@ -5,28 +5,70 @@ public class MadLibs {
   Date: June 21, 2020
   */
   public static void main(String[] args){
-    String name1 = " ";
-    String name2 = " ";
-    //plural
-    String noun1 = " ";
-    //singular
-    String noun2 = " ";
-    //singular
-    String noun3 = " ";
-    //plural
-    String noun4 = " ";
-    //singular
-    String noun5 = " ";
-    //plural
-    String noun6 = " ";
-    String adjective1 = " ";
-    String adjective2 = " ";
-    String adjective3 = " ";
-    String verb1 = " ";
-    String place1 = " ";
-    String number = " ";
+    // pick true or false
+    boolean value = ;
 
-    //Story template
-    String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It's gonna be a "+adjective2+" day! *sigh*' Outside, a bunch of "+noun1+" were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+" very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+" ruled the world.";
+    //names
+    String name1 = "";
+    String name2 = "";
+
+    //plural
+    String noun1 = "";
+    //singular
+    String noun2 = "";
+    //singular
+    String noun3 = "";
+    //plural
+    String noun4 = "";
+    //singular
+    String noun5 = "";
+    //plural
+    String noun6 = "";
+    //singular
+    String noun7 = "";
+    //plural
+    String noun8 = "";
+    //singular
+    String noun9 = "";
+    //plural
+    String noun10 = "";
+
+    //adjectives
+    String adjective1 = "";
+    String adjective2 = "";
+    String adjective3 = "";
+    String adjective4 = "";
+
+    //present tense
+    String verb1 = "";
+    String verb2 = "";
+    String verb3 = "";
+
+    String city = "";
+    String number = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if(value){
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It's gonna be a "+adjective2+" day! *sigh*' Outside, a bunch of "+noun1+" were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+" very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+city+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+" ruled the world. The ruler was a "+noun7+" and all the followers were "+noun8+". "+name1+" organized a group of "+noun10+" to through a coup and "+verb2+". However, before he could "+verb3+", "+name2+" appeared in a "+adjective4+" "+noun9+" and rescued "+name1+"!";
+
+      System.out.println(story);
+    } else {
+      String story = "Last night, "+name1+" had a very "+adjective1+" dream. 'That was "+adjective2+",' "+name1+" said after waking up. *smiles to self in the mirror*' In the dream, a pile of "+noun1+" were stalking a "+noun2+" through a "+noun7+". They began to "+verb1+" and "+verb2+", causing some "+noun4+" to become very "+adjective3+". Strangely, all of a sudden "+name2+" appeared and started to "+verb3+" about "+noun8+". "+city+" went on lockdown immediately and "+name2+" turned into a "+number+"-headed "+noun9+". Just when things couldn't get any more "+adjective4+", a flock of "+noun10+" broke out into song and "+name1+" woke up with a start!";
+
+      System.out.println(story);
+    }
   }
 }
