c91c7a3888b5317156ec7a3e2bd8874f329ad213
grantkee <grant.d.kee@gmail.com>
2020-06-21 12:30:36 -0500

    mad lib template created.

---
+++ MadLibs.java
@@ -0,0 +1,32 @@
+public class MadLibs {
+  /*
+  Mad lib story made in java
+  Author: grantkee
+  Date: June 21, 2020
+  */
+  public static void main(String[] args){
+    String name1 = " ";
+    String name2 = " ";
+    //plural
+    String noun1 = " ";
+    //singular
+    String noun2 = " ";
+    //singular
+    String noun3 = " ";
+    //plural
+    String noun4 = " ";
+    //singular
+    String noun5 = " ";
+    //plural
+    String noun6 = " ";
+    String adjective1 = " ";
+    String adjective2 = " ";
+    String adjective3 = " ";
+    String verb1 = " ";
+    String place1 = " ";
+    String number = " ";
+
+    //Story template
+    String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It's gonna be a "+adjective2+" day! *sigh*' Outside, a bunch of "+noun1+" were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+" very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+" ruled the world.";
+  }
+}
